3a7f24270bb1fe5bda62ee23e508b1a97e2ffa50
Jennifer Kirchner <jkirchne@cswg.com>
2024-06-02 19:48:17 -0400

    drag and drop dynamically

--- src/App.js
+++ src/App.js
@@ -1,90 +1,114 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useDrag } from 'react-dnd';
+import { useDrop } from 'react-dnd';
 import React, { useState } from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import Box from './components/Box.js';
 import NeumorphicButton from './components/NeumorphicButton.js';
-
 
 const boxImage = new URL('./assets/images/box.svg', import.meta.url);
 const gridImage = new URL('./assets/images/grid.svg', import.meta.url);
 const textInputImage = new URL('./assets/images/text_input.svg', import.meta.url);
 
-const App = () => {
-     const [dropProps, setDropProps] = useState({});
-     const [componentProps, setComponentProps] = useState({});
+const componentsMap = {
+     Box,
+     NeumorphicButton
+};
 
 
-     const componentsMap = {
-          Box,
-          NeumorphicButton
-     };
+const handleDrop = (item) => {
+     console.log('Item dropped:', item);
+     // const newDropProps = { ...item };
+     // newDropProps[cellIndex] = {
+     //      type: ComponentType,
+     //      component: <ComponentType props={componentProps} />
+     // };
+     // setDropProps({ ...newDropProps });
+};
 
+const Draggable = ({ id, type, children }) => {
+     const [{ isDragging }, drag] = useDrag(() => ({
+          type,
+          item: { id },
+          collect: (monitor) => ({
+               isDragging: !!monitor.isDragging(),
+          }),
+     }));
 
-     const getAllProps = (Component) => {
-          if (!Component) {
-               throw new Error(`Component ${Component.type} not found.`);
-          }
-          const defaultProps = Component.type.defaultProps || {};
-          console.log('Component', Component, defaultProps);
-          return { ...Component.type, ...Component.children, ...Component.props };
+     return (
+          <div ref={drag} style={{ opacity: isDragging ? 0.5 : 1 }}>
+               {children}
+          </div>
+     );
+};
+
+const Droppable = ({ allowedDropType, onDrop, children }) => {
+     const [droppedItems, setDroppedItems] = useState([]);
+
+     // {
+     //      Object.entries({ ...dropProps }).map(([key, value], ii) => (
+     //           i === parseInt(key, 10) ?
+     //                <div
+     //                     key={ii}
+     //                >
+     //                     <DynamicComponentRenderer componentName={value.component.type}></DynamicComponentRenderer>
+     //                </div> : null
+     //      ))
+     // }
+
+     const [{ isOver }, drop] = useDrop(() => ({
+          accept: allowedDropType,
+          drop: (item, monitor) => {
+               const didDrop = monitor.didDrop();
+               if (!didDrop) {
+                    setDroppedItems(prevItems => [...prevItems, item]);
+                    onDrop(item);
+               }
+          },
+          collect: monitor => ({
+               isOver: !!monitor.isOver({ shallow: true }),
+          }),
+     }), []);
+};
+
+const getAllProps = (Component) => {
+     if (!Component) {
+          throw new Error(`Component ${Component.type} not found.`);
+     }
+     const defaultProps = Component.type.defaultProps || {};
+     console.log('Component', Component, defaultProps);
+     return { ...Component.type, ...Component.children, ...Component.props };
+};
+
+const DynamicComponentRenderer = ({ componentName, props, children, propTypes }) => {
+     const Component = componentsMap[componentName];
+     if (!Component) {
+          return <div>Component not found</div>;
      }
 
-     const DynamicComponentRenderer = ({ componentName, props, children, propTypes }) => {
-          const Component = componentsMap[componentName];
-          if (!Component) {
-               return <div>Component not found</div>;
-          }
+     return <Component {...props} {...propTypes} {...children} {...componentProps}></Component>;
+};
 
-          return <Component {...props} {...propTypes} {...children} {...componentProps}></Component>;
-     };
 
-     const handleDrop = async (event, cellIndex) => {
-          event.preventDefault();
-          const ComponentType = event.dataTransfer.getData('component');
-
-          // Add the component to the grid cell
-          // setComponentProps(await getAllProps(<DynamicComponentRenderer componentName={ComponentType}></DynamicComponentRenderer>));
-          switch (ComponentType) {
-               case 'Box':
-                    setComponentProps(getAllProps(<Box>Insert Text here</Box>));
-                    break;
-               case 'Button':
-                    setComponentProps(getAllProps(<Button>Press Me</Button>));
-                    break;
-               default:
-                    setComponentProps(getAllProps(<Box>What'd You Drop?</Box>));
-                    break;
-          }
-          const newDropProps = { ...dropProps };
-          newDropProps[cellIndex] = {
-               type: ComponentType,
-               component: <ComponentType props={componentProps} />
-          };
-          setDropProps({ ...newDropProps});
-     };
-
-     const handleDragStart = (event, ComponentType) => {
-          event.dataTransfer.setData('component', ComponentType);
-     };
-
+const App = () => {
+     const [dropProps, setDropProps] = useState({});
+     const [componentProps, setComponentProps] = useState({}); 
+     
      const renderGrid = () => {
+          console.log('Marco');
           const grid = [];
-          for (let i = 0; i < 144; i++) {
+          for (let i = 0; i < 15; i++) {
                grid.push(
                     <div
                          key={i}
                          className="grid-item"
-                         onDrop={(event) => handleDrop(event, i)}
-                         onDragOver={(event) => event.preventDefault()}
                     >
-                         {Object.entries({ ...dropProps }).map(([key, value], ii) => (
-                              i === parseInt(key, 10) ? 
-                                   <div 
-                                        key={ii}
-                                   >
-                                        <DynamicComponentRenderer componentName={value.component.type}></DynamicComponentRenderer>
-                                   </div> : null
-                         ))}
+                         <Droppable allowedDropType="BOX" onDrop={handleDrop}>
+                              
+                              Here
+                         </Droppable>
                     </div>
                );
           }
@@ -92,11 +116,19 @@ const App = () => {
      };
 
      return (
-          <div className="app">
-               <div className="app-container">
-                    <div className="grid-container">{renderGrid()}</div>
-                    <div>
+          <DndProvider backend={HTML5Backend}>
+               <div className="app">
+                    <div className="app-container">
+                         <div className="grid-container">
+                              <div style={{ display: 'grid', border: '1px', gridTemplateColumns: 'repeat(12, 12)' }}>
+                                   <Droppable allowedDropType="BOX" onDrop={handleDrop}>
+                                        Drop components here
+                                   </Droppable>
+                              </div>
+                         </div>
                          <div>
+                        
+                         {/* <div>
                               <ul className="component-list">
                                    <li draggable onDragStart={(event) => handleDragStart(event, 'Grid')}>
                                         <img src={gridImage} alt="Grid" />
@@ -111,6 +143,18 @@ const App = () => {
                                         <img src="select-input.svg" alt="Select Input" />
                                    </li>
                               </ul>
+                         </div> */}
+                         <div>
+                              <Draggable id="1" type="BOX">
+                                   {// setComponentProps(getAllProps(<Box>Insert Text here</Box>))
+                                   }
+                                   Box
+                              </Draggable>
+                              <Draggable id="2" type="BOX">
+                                   {// setComponentProps(getAllProps(<NeumorphicButton></NeumorphicButton>))
+                                   }
+                                   Button
+                              </Draggable>
                          </div>
                          <div className="props-div">
                               <strong>{componentProps.type} Properties:</strong>
@@ -127,7 +171,8 @@ const App = () => {
                     </div>
                </div>
           </div>
-     );
-};
+
+          </DndProvider>
+     )};
 
 export default App;
